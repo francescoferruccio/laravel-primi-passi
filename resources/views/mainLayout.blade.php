150617fb58fd64eef1946c85c7a6714472a7876a
Francesco <francesco.ferruccio88@gmail.com>
2020-06-09 15:26:44 +0200

    aggiunto wrapper alla pagina

--- resources/views/mainLayout.blade.php
+++ resources/views/mainLayout.blade.php
@@ -9,19 +9,21 @@
     <script src="/js/app.js" charset="utf-8"></script>
   </head>
   <body>
-    <header>
-      @include('header')
-    </header>
-    <main>
-      <div class="navbar">
-        @include('navbar')
-      </div>
-      <div class="content">
-        @yield('content')
-      </div>
-    </main>
-    <footer>
-      @include('footer')
-    </footer>
+    <div id='container'>
+      <header>
+        @include('header')
+      </header>
+      <main>
+        <div class="navbar">
+          @include('navbar')
+        </div>
+        <div class="content">
+          @yield('content')
+        </div>
+      </main>
+      <footer>
+        @include('footer')
+      </footer>
+    </div>
   </body>
 </html>
