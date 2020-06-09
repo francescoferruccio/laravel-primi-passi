bb8cbae21da4a8b12e8e61d351bdc31d099fe46f
Francesco <francesco.ferruccio88@gmail.com>
2020-06-09 15:33:47 +0200

    link header funzionanti

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <ul>
-  <li>Home</li>
-  <li>About</li>
-  <li>Contatti</li>
+  <li><a href="./home">Home</a></li>
+  <li><a href="./about">About</a></li>
+  <li><a href="./contatti">Contatti</a></li>
 </ul>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,9 +1,9 @@
+Navbar
 <ul>
-  Navbar
-  <li>Link</li>
-  <li>Link</li>
-  <li>Link</li>
-  <li>Link</li>
-  <li>Link</li>
-  <li>Link</li>
+  <li>Link 1</li>
+  <li>Link 2</li>
+  <li>Link 3</li>
+  <li>Link 4</li>
+  <li>Link 5</li>
+  <li>Link 6</li>
 </ul>
